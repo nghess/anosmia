f6e9bf7e89a6f2fd7747600280600af16a1d7a76
Sid-Rafilson-1617 <srafilson@gmail.com>
2025-02-13 14:11:55 -0800

    fixed save

--- ks4_batch.py
+++ ks4_batch.py
@@ -1,4 +1,10 @@
 
+import numpy as np
+import pandas as pd
+from kilosort import run_kilosort
+from kilosort.io import save_preprocessing, load_ops
+from pathlib import Path
+import matplotlib.pyplot as plt
 
 def kilosort(data_path: str, save_path: str, probe_path: str = '8_tetrode.mat', num_channels: int = 32, save_preprocessed: bool = True):
     # Initialize paths
@@ -10,8 +16,23 @@ def kilosort(data_path: str, save_path: str, probe_path: str = '8_tetrode.mat', 
         # Load .npy file and save as binary
         data = np.load(data_path)
         # Ensure data is in (samples, channels) format
-        if data.shape[1] > data.shape[0]:  # If channels > samples, transpose
-            data = data.T
+        #if data.shape[1] > data.shape[0]:  # If channels > samples, transpose
+        print(f"Data import shape:{data.shape}")
+
+        # plt.figure()
+        # plt.title(f'tetrode {1}')
+        # for j in range(4):
+        #     plt.plot(data[j,:30_000 * 10], linewidth = 0.1, alpha = 0.8)
+        # plt.show()
+
+        # If data exceeds abs(max), set to 0
+        #data = data * .1945
+        #data[np.abs(data) > 500] = 0
+
+
+        print(f"Min:{np.min(data)}")
+        print(f"Max:{np.max(data)}")
+
         # Convert to int16 and save as binary
         data = data.T.ravel().astype(np.int16)  # Transpose and flatten for Kilosort format
         temp_bin_path = data_path.parent / 'temp.bin'
@@ -19,13 +40,17 @@ def kilosort(data_path: str, save_path: str, probe_path: str = '8_tetrode.mat', 
         # Create temporary binary file in data parent directory
         data_path = data_path.parent / 'temp.bin'
 
+    # Create results directory if it doesn't exist
+    save_path.mkdir(parents=True, exist_ok=True)
+
     # Run Kilosort 4
-    settings = {'data_dir': data_path.parent, 'n_chan_bin': num_channels}
+    settings = {'data_dir': data_path.parent, 'n_chan_bin': num_channels, 'drift_smoothing': [0, 0, 0]}
     ops, st, clu, tF, Wall, similar_templates, is_ref, est_contam_rate, kept_spikes = \
         run_kilosort(
             settings=settings, 
             probe_name=Path.cwd() / probe_path,
             save_preprocessed_copy=save_preprocessed,
+            do_CAR= False,
             results_dir=save_path
             )
     
