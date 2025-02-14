80a87dc5e4063ea8fa524665b86363f3fa2395bc
Sid-Rafilson-1617 <srafilson@gmail.com>
2025-02-13 22:18:20 -0800

    stable batch v.1

--- ks4_batch.py
+++ ks4_batch.py
@@ -5,10 +5,10 @@ from kilosort import run_kilosort
 from kilosort.io import save_preprocessing, load_ops
 from pathlib import Path
 
-def kilosort(data_path: str, save_path: str, probe_path: str = '8_tetrode.mat', num_channels: int = 32, save_preprocessed: bool = True):
+def kilosort(data_path: str, results_path: str, probe_path: str = '8_tetrode.mat', num_channels: int = 32, save_preprocessed: bool = True):
     # Initialize paths
     data_path = Path(data_path)
-    save_path = Path(save_path)
+    results_path = Path(results_path)
 
     # Handle numpy files by temporarily converting to .bin format   
     if data_path.suffix == '.npy':
@@ -26,7 +26,7 @@ def kilosort(data_path: str, save_path: str, probe_path: str = '8_tetrode.mat', 
         data = np.load(data_path)
 
     # Create results directory if it doesn't exist
-    save_path.mkdir(parents=True, exist_ok=True)
+    results_path.mkdir(parents=True, exist_ok=True)
 
     # Run Kilosort 4
     settings = {'data_dir': data_path.parent, 'n_chan_bin': num_channels, 'Th_universal': 12, 'Th_learned': 11, 'nblocks': 0, 'drift_smoothing': [0, 0, 0], 'dminx': 20}
@@ -36,7 +36,7 @@ def kilosort(data_path: str, save_path: str, probe_path: str = '8_tetrode.mat', 
             probe_name=Path.cwd() / probe_path,
             save_preprocessed_copy=save_preprocessed,
             do_CAR= False,
-            results_dir=save_path
+            results_dir=results_path
             )
     
     # Delete temporary binary file from drive if it exists
@@ -45,7 +45,7 @@ def kilosort(data_path: str, save_path: str, probe_path: str = '8_tetrode.mat', 
         temp_bin_path.unlink()
 
     # Write to 'good' units summary
-    unit_summary(data_path)
+    unit_summary(data_path, results_path)
     
     # Return results
     return ops, st, clu, tF, Wall, similar_templates, is_ref, est_contam_rate, kept_spikes
@@ -61,7 +61,7 @@ def get_mua_paths(directory: str, print_paths=False) -> list:
     paths = [f for f in Path(directory).glob('**/*.npy') if 'mua' in f.name]
     print(f'Found {len(paths)} mua.npy files')
     if print_paths:
-        print_paths(paths)
+        show_paths(paths)
     return paths
 
 # Get all continuous.dat files in a directory and its subdirectories
@@ -69,17 +69,24 @@ def get_continuous_paths(directory: str, print_paths=False) -> list:
     paths = [f for f in Path(directory).glob('**/*.dat') if 'continuous' in f.name]
     print(f'Found {len(paths)} continuous.dat files')
     if print_paths:
-        print_paths(paths)
+        show_paths(paths)
     return paths
 
 # Print collected paths and their indices
-def print_paths(data_paths):
+def show_paths(data_paths):
     for ii, path in enumerate(data_paths):
         print(f"{ii} {path}")
 
 # Grab the number of single units found from kilosort.log and append them to a summary txt file
-def unit_summary(data_path):
-    with open(f"{str(data_path.parent)}/kilosort4/kilosort.log", 'r') as file:
+def unit_summary(data_path, results_path):
+
+    mouse_session = get_savedirs(data_path)
+    savedir = results_path.parents[1]
+    
+    log_file = savedir / mouse_session / "kilosort4.log"
+    output_file = savedir / "good_units.txt"
+
+    with open(log_file, 'r') as file:
         content = file.read()
 
     # Use regex to find the number before "units"
@@ -91,7 +98,10 @@ def unit_summary(data_path):
         num_units = match.group(1)
         
         # Append the number to the output file
-        with open(f"{str(data_path.parent.parent)}units_summary.txt", 'a') as outfile:
-            outfile.write(f'{num_units}\n')
+        with open(output_file, 'a') as outfile:
+            outfile.write(f"{mouse_session} - {num_units} units\n")
     else:
-        print("No matching pattern found in the file")
+        with open(output_file, 'a') as outfile:
+            outfile.write(f"{mouse_session} - No matching pattern found in the file\n")
+
+    print(f"Summary written to {output_file}")
