77c6ad0ef7edc6e8993e5f6949d38e2a79283244
Sid-Rafilson-1617 <srafilson@gmail.com>
2025-02-15 14:43:05 -0800

    noise clipping

--- ks4_batch.py
+++ ks4_batch.py
@@ -5,6 +5,9 @@ from kilosort import run_kilosort
 from kilosort.io import save_preprocessing, load_ops
 from pathlib import Path
 
+"""
+Run kilosort4. Settings dictionary corresponds to kilosort settings.
+"""
 def kilosort(data_path: str, results_path: str, probe_path: str = '8_tetrode.mat', num_channels: int = 32, save_preprocessed: bool = True):
     # Initialize paths
     data_path = Path(data_path)
@@ -15,6 +18,10 @@ def kilosort(data_path: str, results_path: str, probe_path: str = '8_tetrode.mat
         # Load .npy file and save as binary
         data = np.load(data_path)
         print(f"Data import shape:{data.shape}")
+        data_min = data.min()
+        data_max = data.max()
+        data_mean = data.mean()
+        data_std = data.std()
         data = data.reshape(-1, order = 'F')
         temp_bin_path = data_path.parent / 'temp.bin'
         data.tofile(temp_bin_path)
@@ -22,34 +29,67 @@ def kilosort(data_path: str, results_path: str, probe_path: str = '8_tetrode.mat
 
         # Create temporary binary file in data parent directory
         data_path = data_path.parent / 'temp.bin'
-
+      
     else:
         data = np.load(data_path)
+
+    # Take a sliding window (30k samples, 10k sample overlap) and get the np.min and np.max values
+    window_size = 30000
+    overlap = 10000
+    num_windows = (data.shape[0] - window_size) // (window_size - overlap) + 1
+    min_vals = np.zeros(num_windows)
+    max_vals = np.zeros(num_windows)
+    for i in range(num_windows):
+        start = i * (window_size - overlap)
+        end = start + window_size
+        min_vals[i] = np.min(data[start:end])
+        max_vals[i] = np.max(data[start:end])
+    # Get mean of min and max values
+    clip_min_val = np.mean(min_vals)
+    clip_max_val = np.mean(max_vals)
+    three_sigmas = 3 * data.std()
+
+    # If datapoint exceeds 5*max_val, set datapoint to zero
+    data[data > 5*clip_max_val] = 0
+    # If datapoint is less than 5*min_val, set to zero
+    data[data < 5*clip_min_val] = 0
 
     # Create results directory if it doesn't exist
     results_path.mkdir(parents=True, exist_ok=True)
 
-    # Run Kilosort 4
-    settings = {'data_dir': data_path.parent, 'n_chan_bin': num_channels, 'Th_universal': 12, 'Th_learned': 11, 'nblocks': 0, 'drift_smoothing': [0, 0, 0], 'dminx': 20}
-    ops, st, clu, tF, Wall, similar_templates, is_ref, est_contam_rate, kept_spikes = \
-        run_kilosort(
-            settings=settings, 
-            probe_name=Path.cwd() / probe_path,
-            save_preprocessed_copy=save_preprocessed,
-            do_CAR= False,
-            results_dir=results_path
-            )
-    
-    # Delete temporary binary file from drive if it exists
-    temp_bin_path = data_path.parent / 'temp.bin'
-    if temp_bin_path.exists():
-        temp_bin_path.unlink()
+    # Define Kilosort4 settings for the current run
+    settings = {'data_dir': data_path.parent, 'n_chan_bin': num_channels, 'Th_universal': 10, 'Th_learned': 9, 'nblocks': 0, 'drift_smoothing': [0, 0, 0], 'dminx': 20, 'artifact_threshold': np.inf, 'batch_size': 60000}
 
-    # Write to 'good' units summary
-    unit_summary(data_path, results_path)
+    # Run Kilosort 4
+    try:
+        ops, st, clu, tF, Wall, similar_templates, is_ref, est_contam_rate, kept_spikes = \
+            run_kilosort(
+                settings=settings, 
+                probe_name=Path.cwd() / probe_path,
+                save_preprocessed_copy=save_preprocessed,
+                do_CAR= False,
+                results_dir=results_path
+                )
+        
+        # Delete temporary binary file from drive if it exists
+        temp_bin_path = data_path.parent / 'temp.bin'
+        if temp_bin_path.exists():
+            temp_bin_path.unlink()
+
+        # Write to 'good' units summary
+        unit_summary(data_path, results_path, data_min, data_max, data_mean, data_std, clip_min_val, clip_max_val error=False)
+        
+        # Return results
+        return ops, st, clu, tF, Wall, similar_templates, is_ref, est_contam_rate, kept_spikes
     
-    # Return results
-    return ops, st, clu, tF, Wall, similar_templates, is_ref, est_contam_rate, kept_spikes
+    except:
+        # Write error to log
+        unit_summary(data_path, results_path, data_min, data_max, data_mean, data_std, clip_min_val, clip_max_val error=True)
+        return None
+    
+"""
+Helper Functions Below
+"""
 
 # Get the name of the last two directories in data path
 def get_savedirs(path):
@@ -79,7 +119,7 @@ def show_paths(data_paths):
         print(f"{ii} {path}")
 
 # Grab the number of single units found from kilosort.log and append them to a summary txt file
-def unit_summary(data_path, results_path):
+def unit_summary(data_path, results_path, data_min, data_max, data_mean, data_std, clip_min_val, clip_max_val, error=False):
 
     mouse_session = get_savedirs(data_path)
     savedir = results_path.parents[1]
@@ -89,20 +129,23 @@ def unit_summary(data_path, results_path):
 
     with open(log_file, 'r') as file:
         content = file.read()
-
+    
     # Use regex to find the number before "units"
     pattern = r'(\d{1,3}) units found with good refractory periods'
     match = re.search(pattern, content)
 
-    if match:
+    if match and not error:
         # Extract the number from the first capture group
         num_units = match.group(1)
         
         # Append the number to the output file
         with open(output_file, 'a') as outfile:
-            outfile.write(f"{mouse_session} - {num_units} units\n")
+            outfile.write(f"{mouse_session} - {num_units} units - min: {data_min} max: {data_max} mean: {round(data_mean, 3)} std: {round(data_std, 3)} clip_min: {round(clip_min_val, 3)}, clip_max: {round(clip_max_val, 3)}\n")
+    elif error:
+        with open(output_file, 'a') as outfile:
+            outfile.write(f"{mouse_session} - Kilosort failed - min: {data_min} max: {data_max} mean: {round(data_mean, 3)} std: {round(data_std, 3)} clip_min: {round(clip_min_val, 3)}, clip_max: {round(clip_max_val, 3)}\n")
     else:
         with open(output_file, 'a') as outfile:
-            outfile.write(f"{mouse_session} - No matching pattern found in the file\n")
+            outfile.write(f"{mouse_session} - No matching pattern found in the log file\n")
 
     print(f"Summary written to {output_file}")
